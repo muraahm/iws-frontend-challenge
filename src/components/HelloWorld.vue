0712ed57901b8e2d4e77e39aea8f04b73005b0d4
Patrick <p.kenworthy@iwscorp.com>
2022-08-23 12:01:51 -0600

    VUE MQTT template initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,32 @@
+<!-- Paho JS MQTT Docs: https://www.eclipse.org/paho/files/jsdoc/Paho.MQTT.Client.html -->
+
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "test",
+    };
+  },
+  mounted(){
+    this.$root.$on('mqtt-connected', () => {
+      this.$root.mqtt.subscribe('iws-test')
+      this.$root.mqtt.onMessageArrived = (message) => {
+        let topic = message.destinationName
+        let payload = message.payloadString
+        console.log(topic, payload)
+        this.msg = payload
+      }
+      this.$root.mqtt.send('iws-test','mounted')
+    })
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+  render: function (h) {
+    return h(App);
+  },
+  data() {
+    return {
+      mqtt: null
+    }
+  },
+  created() {
+    let host = 'test.mosquitto.org';
+    let port = 8080;
+
+    console.log("Connecting to " + host + " " + port);
+    this.mqtt = new Paho.MQTT.Client(host, port, "clientjs");
+    let options = {
+      timeout: 3,
+      onSuccess: (() => {
+          console.log("Connected");
+          this.$emit('mqtt-connected', true)
+        })
+    };
+    this.mqtt.connect(options); //connect
+  }
+}).$mount("#app");
