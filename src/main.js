22a66326caed3e09bab631450dcd34937b5fe45a
Ahmed Murad <amurad@Ahmeds-MacBook-Pro.local>
2023-02-27 17:14:19 -0700

    chore: code formatting and import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from "vue";
-import App from "./App.vue";
+import App from "./App";
 import IWS_MQTT from "./mqtt"
+
+// Bootstrap
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 new Vue({
@@ -10,15 +20,14 @@ new Vue({
   data() {
     return {
       mqtt: null
-    }
+    };
   },
   created() {
-    const HOST = 'test.mosquitto.org';
+    const HOST = "test.mosquitto.org";
     const PORT = 8080;
     this.mqtt = new IWS_MQTT(HOST, PORT);
     this.mqtt.connect(() => {
-      this.$emit('mqtt-connected', true)
-    })
-
+      this.$emit("mqtt-connected", true);
+    });
   }
 }).$mount("#app");
